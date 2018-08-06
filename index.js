30cccd0fad602c772aa6c5d8ed6810021c8f78e1
John Papa <john@johnpapa.net>
2018-08-06 19:19:32 -0400

    node hello

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
