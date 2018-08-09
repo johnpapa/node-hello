da29164e15f07c5d205713180bc8467298818fb1
John Papa <john@johnpapa.net>
2018-08-09 12:58:46 -0400

    github deploy!

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello Github Deploy!\n');
 });
 
 server.listen(port, () => {
