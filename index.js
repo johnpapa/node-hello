e8f7a7bc37d110bc83be2df53dcd592985c685ac
Canh Nguyen <phongcanhit@gmail.com>
2020-08-30 09:54:47 +0000

    Add Dockerfile for app build

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+const app_env = process.env.APP_ENV || "unknown";
+const app_version = process.env.APP_VERSION || "unknown";
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  //const msg = 'Hello Node!\n'
+  const msg = `Hello Word! Application version ${app_version} - Environment ${app_env}\n`
   res.end(msg);
 });
 
