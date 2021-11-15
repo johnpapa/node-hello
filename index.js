2c388a0b6007a42d8c1c0b6759bc0843822c43f2
Akankshaanshu <89064929+Akankshaanshu@users.noreply.github.com>
2021-11-15 13:01:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 
-const path = require('path')
 
 
 const server = http.createServer((req, res) => {
-   res.sendFile(path.join(__dirname, 'index.html'))
+   
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
