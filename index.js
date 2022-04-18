103f11cf208483fd2ba697728aff97ebbe554812
Mahmoud Anwer <mahmoudanwer071@gmail.com>
2022-04-19 01:46:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello World!\n'
   res.end(msg);
 });
 
