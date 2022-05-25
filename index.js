9aec89cad78f65c752e5e95cb4257e7aebccc9c0
qasim hussain nasir <38753395+qas52@users.noreply.github.com>
2022-05-25 02:42:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+const assert = require('chai').assert;
+const Chef = require('../app');
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+describe('Chef test', function(){
+   
+    let chef = Chef;
+    
+    it('check the dish has valid name.', function(){
+        assert.isString(chef.checkMenu(), 'string');
+    })
+    
+    it('check for a dish in menu.', function (){
+        let dish= chef.checkMenu()
+        assert.oneOf(dish, chef.dishes)
+
+    });
+    
 });
