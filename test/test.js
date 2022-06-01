ee53b433255882369e554daa18efefa4cb21bee2
qasim hussain nasir <38753395+qas52@users.noreply.github.com>
2022-06-01 06:41:04 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
+
 const assert = require('chai').assert;
-const Chef = require('../index');
+const Chef = require('../app');
 
 describe('Chef test', function(){
    
@@ -7,21 +8,12 @@ describe('Chef test', function(){
     
     it('check the dish has valid name.', function(){
         assert.isString(chef.checkMenu(), 'string');
-    });
+    })
     
     it('check for a dish in menu.', function (){
         let dish= chef.checkMenu()
         assert.oneOf(dish, chef.dishes)
 
-    });
-
-    it('make sure the chef can feed more.', function(){
-
-        for(var i=0; i<6; i++){
-            chef.customersFed();
-            assert.isAtLeast(chef.customers, 0); 
-        }
-        
     });
     
 });
