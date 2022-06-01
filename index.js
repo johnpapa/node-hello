7128b99b8958d86ac539e5573b22c2e054a4737b
qasim hussain nasir <38753395+qas52@users.noreply.github.com>
2022-06-01 06:40:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var Chef = function(){
     this.dishes = ['Dosa', 'Tea', 'Pokoras', 'Pav Bhaji'];
-    this.customers= 5;
 };
 Chef.prototype.checkMenu= function(){
     //returns a string
@@ -9,23 +8,6 @@ Chef.prototype.checkMenu= function(){
 
     console.log("I will like to have:", dish);
     return dish;
-};
-Chef.prototype.customersFed= function(){
-
-    if(this.customers >=1){
-
-        console.log("Customer fed with yummy food!");
-        this.customers--;
-
-    }else if(this.customers ==0){
-        console.log("All customers have been satisfied. Done for the day!");
-    }else{
-        let cusLeft= this.customers;
-        console.log("Customer fed with yummy food!", cusLeft , "more to go!");
-        this.customers-= cusLeft;
-    }
-
-    return this.customers;  
 };
 
 let chef = new Chef();
