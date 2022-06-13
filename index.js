2ddf55bd30eb27d31029def034d0a3f4e96d0ee5
jaga <99672276+8971185447@users.noreply.github.com>
2022-06-13 15:14:59 +0530

    NEW Changes
    Signed-off-by: jaga <99672276+8971185447@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'spexmojoworking mjiijijij12345678989711854471234567890!\n'
+  const msg = 'spexmojoworking!\n'
   res.end(msg);
 });
 
