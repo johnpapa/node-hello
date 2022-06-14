77c3f1b123978e1886e10e660d4a7607a834d07b
jaga <99672276+8971185447@users.noreply.github.com>
2022-06-14 13:22:43 +0530

    Update index.js
    Signed-off-by: jaga <99672276+8971185447@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'spexmojoworking 1234567890 iam working on this !\n'
+  const msg = 'spexmojo!\n'
   res.end(msg);
 });
 
-server.listen , () => {
-  console.log(`Server running on http://50.18.134.35:8080/');
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
+
