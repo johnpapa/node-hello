006dc965e4e82f04493e69ef8713f41f2dbe3798
jaga <99672276+8971185447@users.noreply.github.com>
2022-06-14 13:19:18 +0530

    new
    Signed-off-by: jaga <99672276+8971185447@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
   res.end(msg);
 });
 
-server.listen(8080, () => {
-  console.log(`Server running on http://50.18.134.35:$8080/`);
+server.listen , () => {
+  console.log(`Server running on http://50.18.134.35:8080/');
 });
