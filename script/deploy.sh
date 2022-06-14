20fee2aa0a033017e43e954a706d48cc9bdf4147
jaga <99672276+8971185447@users.noreply.github.com>
2022-06-14 17:02:43 +0530

    Update deploy.sh
    Signed-off-by: jaga <99672276+8971185447@users.noreply.github.com>

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,9 @@
-ssh -i /home/ubuntu/node.js.pem ubuntu@54.215.214.117 <<EOF
-sudo su
-sudo git pull
-git pull
-npm install  - production
-npm install  
-pm2 restart all
+#!/bin/sh
+ssh ubuntu@54.215.214.117 <<EOF
+ cd ~/node-hello
+ git pull
+ npm install — production
+ pm2 restart all
+ exit
+EOF
 
