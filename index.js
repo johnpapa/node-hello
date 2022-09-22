415d6913e273d7bdb0300e31a6cf4a8ebe2575a9
mahmoudmohamed22 <mahmoudmohamed5555@yahoo.com>
2022-09-22 21:02:59 +0200

    second modification

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node hello DevOPs Team !\n'
+  const msg = 'Hello Node hello DevOPs Team and more mkdsnamflmmdnmanf !\n'
   res.end(msg);
 });
 
