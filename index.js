d570c30bd8a6c8c63c36fa4a1bcd5385e47b2776
Yasser Magdy <47793179+YasserElmenshawy@users.noreply.github.com>
2022-09-23 05:54:37 +0200

    change the message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!  \n test2'
+  const msg = 'Hellooooo to  Node!  \n test2'
   res.end(msg);
 });
 
