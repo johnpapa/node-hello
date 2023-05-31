3b71cb09507589668a023cf32597e5577243d751
Gabriel Daltoso <ggdaltoso@gmail.com>
2023-05-31 11:16:09 -0300

    web-publisher: Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  res.end(msg);
+
+  const env = JSON.stringify(process.env, null, 2)
+  
+  res.end(`${msg}\n\n${env}`);
 });
 
 server.listen(port, () => {
